b079cdbeb021a796bcbf1c621f4c59def0488a7f
juyeongkim <kimjuyeong2@gmail.com>
2025-06-19 11:12:18 +0900

    modify: 차트 색상 변경

--- src/components/CandleChart.tsx
+++ src/components/CandleChart.tsx
@@ -1,9 +1,10 @@
 'use client'
 
-import React, { useEffect } from "react";
+import React, { useEffect, useRef } from "react";
 import dynamic from 'next/dynamic';
 
 import { useCandleStore } from "@/store/candleStore";
+import { useMarketStore } from "@/store/marketStore";
 
 
 // 마켓 코드, 3분봉 차트 예시 하드 코딩
@@ -17,22 +18,70 @@ const ChartComponent = dynamic(() => import("@/lib/chartUtils").then(mod => mod.
 });
 
 export const CandleChart = () => {
-  const { candles, error, fetchCandles } = useCandleStore();
+  const { candles, error, selected_market, selected_time, set_selectedTime,  set_selectedMarket, fetchCandles } = useCandleStore();
+  const  { markets, initializeMarkets }  = useMarketStore();
+  const canvasRef = useRef<HTMLCanvasElement>(null);
+
 
   useEffect(() => {
-    fetchCandles(MARKET_CODE, TIME, CNT);
-  }, [fetchCandles]);
+    initializeMarkets();
+  }, [initializeMarkets]);
 
+  useEffect(() => {
+    if(selected_market) {
+      console.log(selected_time, '히히');
+      fetchCandles(selected_market, selected_time.time, selected_time.cnt);
+    }
+  }, [selected_market, selected_time]);
+
+  const selectedMarket = (e: React.ChangeEvent<HTMLSelectElement>) => {
+    set_selectedMarket(e.target.value);
+  };
+
+  const selectedTime = (e: React.ChangeEvent<HTMLSelectElement>) => {
+    const selected = e.target.options[e.target.selectedIndex];
+    const selectedCnt = selected.getAttribute("date-cnt");
+
+    set_selectedTime(selected.value.slice(0, 7), Number(selectedCnt));
+  }
+  
   if (error) {
     return <div>Error: {error}</div>;
   }
 
   return (
     <div>
-      <h2>캔들 차트</h2>
       
-      <canvas id="candle-chart" width={800} height={400}></canvas>
-      <ChartComponent market={MARKET_CODE} candle={candles}></ChartComponent>
+      
+      <div className="flex tiems-center gap-4 p-4 bg-gray-100 border rounded-lg shadow-lg"> 
+        <div className="relative">
+          <select className="px-4 py-2 border rounded-md bg-white text-gray-700 hover:border-gray-400 focus:outline-none focus:ring focus:ring-blue-200" value={selected_market} onChange={selectedMarket}>
+            {markets.map((market) => (
+              <option key={market.market} value={market.market}>
+                {market.korean_name}
+              </option>
+            ))}
+          </select>
+        </div>
+
+        <div className="relative">
+          <select className="px-4 py-2 border rounded-md bg-white text-gray-700 hover:border-gray-400 focus:outline-none focus:ring focus:ring-blue-200" onChange={selectedTime} defaultValue={'minutes_30'}>
+            <option key={'second'} value={'seconds'} date-cnt={0}>초</option>
+            <option key={'minute_1'} value={'minutes_1'} date-cnt={1}>1분</option>
+            <option key={'minute_3'} value={'minutes_3'} date-cnt={3}>3분</option>
+            <option key={'minute_5'} value={'minutes_5'} date-cnt={5}>5분</option>
+            <option key={'minute_10'} value={'minutes_10'} date-cnt={10}>10분</option>
+            <option key={'minute_15'} value={'minutes_15'} date-cnt={15}>15분</option>
+            <option key={'minute_30'} value={'minutes_30'} date-cnt={30} >30분</option>
+            <option>1시간</option>
+
+          </select>
+        </div>
+      </div>
+      
+      <canvas ref={canvasRef} id="candle-chart" width={800} height={400}></canvas>
+
+      <ChartComponent market={selected_market} candle={candles} canvasRef={canvasRef}></ChartComponent>
       
     </div>
   )
--- src/lib/chartUtils.ts
+++ src/lib/chartUtils.ts
@@ -15,14 +15,6 @@ Chart.register(
   // 그 외... 등록할 요소들
 )
 
-declare module "chart.js" {
-    interface ChartDatasetProperties<TType extends ChartType, TData> {
-        upColor?: string; // 상승 캔들의 색상
-        downColor?: string; // 하락 캔들의 색상
-        borderColor?: string; // 캔들의 테두리 색상
-    } 
-}
-
 interface Candle {
     x: number; // 시간 (timestamp)
     o: number; // 시가
@@ -31,30 +23,29 @@ interface Candle {
     c: number; // 종가
 }
 
-type list = Candle[];
-
 interface WriteChartProps {
-    market: string; // 마켓 코드
-    candle: list; // 캔들 데이터 배열
+    market: string;
+    candle: Candle[];
+    canvasRef: React.RefObject<HTMLCanvasElement>;
 }
 
 
 // candlestick 차트
-const WriteChart: React.FC<WriteChartProps> = ({ market, candle }) => {
+const WriteChart: React.FC<WriteChartProps> = ({ market, candle, canvasRef }) => {
     
     useEffect(() => {
 
-        console.log(candle);
-
         // 캔버스 요소 가져오기
-        const ctx = document.getElementById('candle-chart') as HTMLCanvasElement;
+        const ctx = canvasRef.current;
+        
         
         // 캔버스가 존재하지 않으면 종료
         if (!ctx) return;
 
-        if (Chart.getChart(ctx)) {
-            // 이미 차트가 존재하면 제거
-            Chart.getChart(ctx)?.destroy();
+        // 이미 차트가 존재하면 제거
+        const exisChart = Chart.getChart(ctx);
+        if(exisChart) {
+            exisChart.destroy();
         }
 
         // console.log("차트에 들어가는 data:", candle);
@@ -68,9 +59,16 @@ const WriteChart: React.FC<WriteChartProps> = ({ market, candle }) => {
                     {
                         label: market,
                         data: candle,
-                        upColor: "rgba(200, 0, 0, 0.8)",
-                        downColor: "rgba(0, 13, 200,0.8)",
-                        borderColor: "rgba(0, 0, 0, 0.8)",
+                        borderColors: {
+                            up: 'rgba(200, 0, 13, 0.8)',
+                            down: 'rgba(0, 0, 200, 0.8)',
+                            unchanged: 'rgba(143, 143, 143, 1)'
+                        },
+                        backgroundColors: {
+                            up: 'rgba(200, 0, 13, 0.8)',
+                            down: 'rgba(0, 0, 200, 0.8)',
+                            unchanged: 'rgba(143, 143, 143, 1)'
+                        }
                     },
                 ],
             },
@@ -97,17 +95,19 @@ const WriteChart: React.FC<WriteChartProps> = ({ market, candle }) => {
                 },
                 plugins: {
                     tooltip: {
-                        callbacks: {
-                            label: function (context: any) {
-                                const { o, h, l, c } = context.raw;
-                                return `시가: ${o}, 고가: ${h}, 저가: ${l}, 종가: ${c}`;
-                            },
-                        },
+                        mode: 'nearest',
+                        intersect: true,
+                        // callbacks: {
+                        //     label: function (context: any) {
+                        //         const { o, h, l, c } = context.raw;
+                        //         return `시가: ${o}, 고가: ${h}, 저가: ${l}, 종가: ${c}`;
+                        //     },
+                        // },
                     },
                 },
             },
     });
-}, []);
+}, [market, candle]);
 
     return null;
 };
