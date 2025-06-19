30905224298622f0f6f2b739a64a942ba0fca43a
juyeongkim <kimjuyeong2@gmail.com>
2025-06-19 11:11:33 +0900

    add: market_code, time 추가

--- src/store/candleStore.ts
+++ src/store/candleStore.ts
@@ -9,15 +9,28 @@ interface Candle {
     c: number; // 종가
 }
 
+interface Selected_time {
+    time: string;
+    cnt: number | null;
+}
+
 interface CandleStoreState {
     candles: Candle[];
     error: string | null;
+    selected_market: string;
+    selected_time: Selected_time;
+
     fetchCandles: (marketCode: string, time: string, timeCnt: number) => Promise<void>;
+    set_selectedMarket: (selected_market: string) => Promise<void>;
+    set_selectedTime: (time: string, cnt: number | null) => Promise<void>
 }
 
 export const useCandleStore = create<CandleStoreState>((set) => ({
     candles: [],
     error: null,
+    selected_market: 'KRW-BTC',
+    selected_time: { time: 'minutes', cnt: 30 },
+
 
     fetchCandles: async (marketCode, time, timeCnt) => {
         set({ error: null });
@@ -26,7 +39,7 @@ export const useCandleStore = create<CandleStoreState>((set) => ({
             if (!response || !response.data) {
                 throw new Error("No data received from API");
             }
-            
+
             // candle 데이터 형식에 맞게 변환
             const candles = response.data.map((candle: any) => ({
                 x: new Date(candle.candle_date_time_kst).getTime(),
@@ -39,5 +52,13 @@ export const useCandleStore = create<CandleStoreState>((set) => ({
         } catch (error: unknown) {
             set({ error: error instanceof Error ? error.message : String(error) });
         }
+    },
+
+    set_selectedMarket: async (market: string) => {
+        set({ selected_market: market });
+    },
+
+    set_selectedTime: async (time: string, cnt: number | null) => {
+        set({ selected_time: { time, cnt } });
     }
 }));
