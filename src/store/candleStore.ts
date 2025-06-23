177b1ef70ecd4be638ec9b572bc36773c6e25261
juyeongkim <kimjuyeong2@gmail.com>
2025-06-23 09:26:58 +0900

    modify: 각 봉별로 데이터 약 2000개씩 가져오는 걸로 변경

--- src/store/candleStore.ts
+++ src/store/candleStore.ts
@@ -14,18 +14,19 @@ interface Selected_time {
     cnt: number | null;
 }
 
+type TimeUnit = 'millisecond' | 'second' | 'minute' | 'hour' | 'day' | 'week' | 'month' | 'quarter' | 'year';
+
 interface CandleStoreState {
     candles: Candle[];
     error: string | null;
     selected_market: string;
     selected_time: Selected_time;
-    timeUnit: string;
+    timeUnit: TimeUnit;
 
-    fetchCandles: () => Promise<void>;
     fetchAdditionCandles: () => Promise<void>;
     set_selectedMarket: (selected_market: string) => Promise<void>;
-    set_selectedTime: (time: string, cnt: number | null) => Promise<void>
-    set_timeUnit: (timeUnit: string) => Promise<void>
+    set_selectedTime: (time: string, cnt: number | null) => Promise<void>;
+    set_timeUnit: (timeUnit: TimeUnit) => Promise<void>;
 }
 
 async function delay(ms: any) {
@@ -39,38 +40,10 @@ export const useCandleStore = create<CandleStoreState>((set, get) => ({
     selected_time: { time: 'minutes', cnt: 30 },
     timeUnit: 'hour',
 
-
-    fetchCandles: async () => {
-        
-        const market_code = get().selected_market;
-        const { time, cnt } = get().selected_time;
-        
-        set({ error: null });
-        try {
-            const response = await getCoin.getCandles({ marketCode: market_code, time: time, timeCnt: cnt });
-            if (!response || !response.data) {
-                throw new Error("No data received from API");
-            }
-
-            // candle 데이터 형식에 맞게 변환
-            const candles = response.data.map((candle: any) => ({
-                x: new Date(candle.candle_date_time_kst).getTime(),
-                o: candle.opening_price,
-                h: candle.high_price,
-                l: candle.low_price,
-                c: candle.trade_price,
-            }));
-            set({ candles });
-        } catch (error: unknown) {
-            set({ error: error instanceof Error ? error.message : String(error) });
-        }
-    },
-
     fetchAdditionCandles: async () => {
 
         const market_code = get().selected_market;
         const { time, cnt } = get().selected_time;
-        const week = Date.now() - 7 * 24 * 60 * 60 * 1000;
 
         let allCandles: Candle[] = [];
         let toDatetime = new Date().toISOString();
@@ -78,6 +51,8 @@ export const useCandleStore = create<CandleStoreState>((set, get) => ({
         set ({ error: null });
 
         try {
+            let call_cnt = 0;
+
             while(true) {
                 const response = await getCoin.getAdditionCandles({
                     marketCode: market_code,
@@ -104,9 +79,8 @@ export const useCandleStore = create<CandleStoreState>((set, get) => ({
                 const oldtime = candles[0];
                 toDatetime = new Date(oldtime.x).toISOString();
 
-                if (oldtime.x < week) break;
-
-                if (candles.length < 200) break;
+                call_cnt += 1
+                if (call_cnt >= 5) break;
 
                 await delay(100); // 지연
 
@@ -120,7 +94,7 @@ export const useCandleStore = create<CandleStoreState>((set, get) => ({
             // 중복 제거 및 정렬
             const resultCandles = Array.from(
                 new Map(updateCandles.map((c) => [c.x, c])).values()
-            );
+            ).sort((a, b) => (a.x - b.x));
 
 
             set({ candles: resultCandles });
@@ -138,7 +112,7 @@ export const useCandleStore = create<CandleStoreState>((set, get) => ({
         set({ selected_time: { time, cnt } });
     },
 
-    set_timeUnit: async (timeUnit: string) => {
+    set_timeUnit: async (timeUnit: TimeUnit) => {
         set({ timeUnit: timeUnit });
     },
 }));
