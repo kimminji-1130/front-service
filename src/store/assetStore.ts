67218b7886feada7c61d4755185fcd7b6ddddca2
juyeongkim <kimjuyeong2@gmail.com>
2025-06-25 14:49:48 +0900

    add: 도넛형 차트에 사용할 데이터 추가

--- src/store/assetStore.ts
+++ src/store/assetStore.ts
@@ -1,5 +1,4 @@
 import { create } from 'zustand';
-import { useEffect, useState } from 'react';
 
 import { useMarketStore } from './marketStore';
 
@@ -17,7 +16,8 @@ interface AssetState {
   holdings: number;
 
   getCurrentPrice: (market: string) => number;
-  getTotalValuation: (assets: Asset[], holdings: number) => number[];
+  getTotalValuation: (assets: Asset[]) => number[];
+  getDoughnutData: (assets: Asset[]) => { label: string; data: number; }[];
 }
 
 export const useAssetStore = create<AssetState>((set, get) => ({
@@ -32,42 +32,44 @@ export const useAssetStore = create<AssetState>((set, get) => ({
         {
             market_code: 'KRW-ETH',
             market_name: '이더리움',
-            total_coin_price: 409482,
-            total_coin_cnt: 0.123,
+            total_coin_price: 409482000,
+            total_coin_cnt: 10,
         },
         {
             market_code: 'KRW-USDT',
             market_name: '테더',
-            total_coin_price: 409482,
+            total_coin_price: 40948200000,
             total_coin_cnt: 0.123,
         },
         
     ],
-    holdings: 100000000,
+    holdings: 0,
 
     // 현재가 가져오기
     getCurrentPrice: (market: string) => {
-        const { tickers, error } = useMarketStore();
-
+        const { tickers } = useMarketStore();
         const price = tickers[market]?.trade_price;
-        return price;
+
+        if (price) {
+            return price;
+        }
+        return 0;
     },
 
-    getTotalValuation: (assets: Asset[], holdings: number) => {
-        const {getCurrentPrice} = get();
-        const [ total_price, setTotal_price ] = useState(0);
+    getTotalValuation: (assets: Asset[]) => {
+        const {getCurrentPrice, holdings} = get();
 
-        useEffect(() => {
-            let total = assets.reduce((sum, asset) => sum + asset.total_coin_price, 0);
-            setTotal_price(total);
-        }, [assets]);
+        // 총 매수 코인 가격
+        const total_price = assets.reduce((sum, asset) => sum + asset.total_coin_price, 0);
 
+        // 현재가 x 보유 코인 개수
         let valuations = assets.map((asset) => getCurrentPrice(asset.market_code) * asset.total_coin_cnt);
-        let total_valuations = valuations.reduce((sum, price) => sum + price, 0);
+        let total_valuations = valuations.reduce((sum, price) => sum + price, 0); // 총평가
 
         let total_holding = total_valuations + holdings; // 총 보유자산
         let pl = total_valuations - total_price; // 평가손익
         let total_rateReturn = pl/total_price * 100; // 총 수익률
+
         
 
         let result = [total_price, total_valuations, total_holding, pl, total_rateReturn];
@@ -75,4 +77,18 @@ export const useAssetStore = create<AssetState>((set, get) => ({
         return result;   
     },
 
+    getDoughnutData: (assets: Asset[]) => {
+        const { getCurrentPrice, getTotalValuation } = get();
+        const result = getTotalValuation(assets);
+        
+        const data = assets.map((asset) => {
+            // asset.market_code로 현재가를 가져와서 계산
+            const current = getCurrentPrice(asset.market_code);
+            return {
+                label: asset.market_name,
+                data: Number((current * asset.total_coin_cnt) / result[1]) * 100,
+            }
+        })
+        return data;
+    },
 }))
